d0b70b1781f063d6f27f43ad7c44de0b02536313
DadonovAD <DadonovAD@tvv.transneft.ru>
2021-11-06 20:20:26 +0300

    Init

---
+++ SumOfDigit_DigitalRoot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumOfDigit_DigitalRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
