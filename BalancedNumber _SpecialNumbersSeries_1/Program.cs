dd27693923651eee526615c1ac577daea58c9ddd
DadonovAD <DadonovAD@tvv.transneft.ru>
2021-11-11 21:02:54 +0300

    Kata_2: Balanced Number Special Numbers Series 1 - Init

---
+++ BalancedNumber _SpecialNumbersSeries_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BalancedNumber__SpecialNumbersSeries_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definition
+            // Balanced number is the number that * The sum of all digits to the left of the middle digit(s)
+            // and the sum of all digits to the right of the middle digit(s) are equal*.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
