78847e6f0d9b71f9852f711fbfbd5fcd6eac6234
DadonovAD <DadonovAD@tvv.transneft.ru>
2022-09-10 15:19:06 +0300

    Kata 7. Best answer.

--- Roman Numerals Encoder_6_kyu/Program.cs
+++ Roman Numerals Encoder_6_kyu/Program.cs
@@ -52,3 +52,19 @@ static string Solution(int n)
     if (n >= 1) return "I" + Solution(n - 1);
     return "";
 }
+
+static string Solution2(int n)
+{
+    string roman = "";
+    string[] thousand = {"","M","MM","MMM"};
+    string[] hundred = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+    string[] ten = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+    string[] one = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+  
+    roman += thousand[(n/1000)%10];
+    roman += hundred[(n/100)%10];
+    roman += ten[(n/10)%10];
+    roman += one[n%10];
+  
+    return roman;
+}
